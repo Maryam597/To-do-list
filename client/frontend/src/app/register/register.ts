af8d6ade22852bfd01bf101195b81af09b0e0c20
Maryam597 <maryam.dri@hotmail.fr>
2025-07-18 23:28:21 +0200

    register form

--- client/frontend/src/app/register/register.ts
+++ client/frontend/src/app/register/register.ts
@@ -1,11 +1,48 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [    
+    CommonModule,
+    FormsModule,
+    // HttpClientModule,
+    RouterModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule
+],
   templateUrl: './register.html',
   styleUrl: './register.scss'
 })
 export class Register {
+  registerData = {
+    username: '',
+    email: '',
+    password: '',
+  };
 
+  errorMessage = '';
+  successMessage = '';
+
+  constructor(private http: HttpClient) {}
+
+  onSubmit() {
+    this.http.post('http://localhost:8080/api/users/register', this.registerData).subscribe({
+      next: (res) => {
+        this.successMessage = "Inscription réussie !";
+        this.errorMessage = '';
+      },
+      error: (err) => {
+        this.errorMessage = err.error;
+        this.successMessage = '';
+      },
+    });
+  }
 }
