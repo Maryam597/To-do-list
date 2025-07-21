9299ce395b2cefe2910a60dff9b6066129bd2857
Maryam597 <maryam.dri@hotmail.fr>
2025-07-21 17:46:16 +0200

    Task duedate -ongoing debug

--- client/frontend/src/app/services/task.service.ts
+++ client/frontend/src/app/services/task.service.ts
@@ -9,30 +9,23 @@ import { Auth } from '../auth/auth';
 })
 export class TaskService {
 
-  private apiUrl = 'http://localhost:8080/api/tasks';  // L'URL de ton backend Java
+  private apiUrl = 'http://localhost:8080/api/tasks';  
 
-  constructor(private http: HttpClient, private auth: Auth) { }
-
-    private getHeaders(): HttpHeaders {
-      return new HttpHeaders({
-        Authorization: `Bearer ${this.auth.getToken()}`
-      });
-    }
-  
+  constructor(private http: HttpClient) { }
 
   getTasks(): Observable<Task[]> {
-    return this.http.get<Task[]>(this.apiUrl, { headers: this.getHeaders()});
+    return this.http.get<Task[]>(this.apiUrl);
   }
 
   addTask(task: Task): Observable<Task> {
-    return this.http.post<Task>(this.apiUrl, task, { headers: this.getHeaders()});
+    return this.http.post<Task>(this.apiUrl, task);
   }
 
   deleteTask(id: number): Observable<void> {
-    return this.http.delete<void>(`${this.apiUrl}/${id}`, { headers: this.getHeaders()});
+    return this.http.delete<void>(`${this.apiUrl}/${id}`);
   }
 
   updateTask(task: Task): Observable<Task> {
-    return this.http.put<Task>(`${this.apiUrl}/${task.id}`, task, { headers: this.getHeaders()});
+    return this.http.put<Task>(`${this.apiUrl}/${task.id}`, task);
   }
 }
--- client/frontend/src/main.ts
+++ client/frontend/src/main.ts
@@ -2,15 +2,20 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app';
 import { provideRouter } from '@angular/router';
 import { routes } from './app/app.routes';
-import { provideHttpClient } from '@angular/common/http';  // <-- ici
+import { provideHttpClient, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { importProvidersFrom } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AuthInterceptor } from './app/auth.interceptor';
 
 bootstrapApplication(AppComponent, {
   providers: [
     provideRouter(routes),
     provideHttpClient(),
-        importProvidersFrom(BrowserAnimationsModule)
-
+    importProvidersFrom(BrowserAnimationsModule),
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptor,
+      multi: true
+    }
   ]
 });
